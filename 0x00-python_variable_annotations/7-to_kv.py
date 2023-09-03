bc68f3b684d996f187839f884224fe6bd1dba52f
BAlogun Adedotun <Adedotun_balogun@yahoo.com>
2023-09-03 18:28:15 +0100

    7-to_kv.py

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+"""
+sum use of float
+"""
+from typing import Tuple, Union
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    sum use of float
+    """
+    return (k, float(v * v))

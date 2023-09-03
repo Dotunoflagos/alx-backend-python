8befc82c2d3f4e1a0b1e1a48c558f28317c59237
BAlogun Adedotun <Adedotun_balogun@yahoo.com>
2023-09-03 18:40:32 +0100

    8-make_multiplier.py

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -4,6 +4,7 @@ sum use of float
 """
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Returns a function that multiplies a float by the given multiplier."""
     def multiplier_function(x: float) -> float:

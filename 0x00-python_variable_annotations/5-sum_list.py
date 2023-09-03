32e01123f5b91720057c5dceffa84e5b02690e40
BAlogun Adedotun <Adedotun_balogun@yahoo.com>
2023-09-03 18:01:51 +0100

    5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+sum use of float
+"""
+from typing import List
+
+
+def sum_list(imput_list: List[float]) -> float:
+    """
+    sum use of float
+    """
+    return sum(input_list)

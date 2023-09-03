791ecef5afe1cfa7bb044c2780d1df743249d7e5
BAlogun Adedotun <Adedotun_balogun@yahoo.com>
2023-09-03 17:48:21 +0100

    3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+to_str: use to mat.floor to return int
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    to_str: use to mat.floor to return int
+    """
+    return str(n)
